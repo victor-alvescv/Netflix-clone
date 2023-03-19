3ac86742a6293e57ca31f5d89840ff5984b4b05d
victor-alvescv <victoralvescabral7@gmail.com>
2023-03-19 20:26:18 -0300

    Third Commit

--- Note.js
+++ Note.js
@@ -1,8 +1,31 @@
-/* LIBRARYS AND SOFTWARES USED: 
+/* Used Extensions:
 
+. Tailwind CSS (Library)
 
+. Next.js (Software)
 
+. HeroIcon (Library)
 
+. React Icons (Library)
 
+. TMDB Api (Database)
+
+. Firebase (Host)
+
+. Stripe (Software)
+
+. Vercel (Cloud Platform)
+
+. React Hook Form (Library)
+
+. Recoil (library)
+
+. Material Ui (library)
+
+. React Player (React component)
+
+. Stripe Firestore (extension - Firebase)
+
+. React Hot Toast (library)
 
 */
--- pages/index.tsx
+++ pages/index.tsx
@@ -119,5 +119,3 @@ export const getServerSideProps = async () => {
     }
   }
 }
-
-// ORGANIZAR OS ARQUIVOS, LIBRARYS/SOFTWARES, VERCEL, PORTFOLIO
